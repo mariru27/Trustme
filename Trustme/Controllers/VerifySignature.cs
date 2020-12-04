a6923bb3d627c75190cf3c3e8c037d7f60791250
mariru27 <marina.rusu.99@gmail.com>
2020-12-04 13:18:04 +0200

    verify signature, view and action

---
+++ Trustme/Controllers/VerifySignature.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Trustme.Controllers
+{
+    public class VerifySignature : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult VerifySign()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string VerifySignatureDocument(string username, string signature, IFormFile document)
+        {
+            if(ModelState.IsValid)
+            {
+                return "is valid";
+            }
+            return "not valid";
+        }
+    }
+}
