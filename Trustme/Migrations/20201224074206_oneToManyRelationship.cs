efb6c59922cd921123a14d7ac2712cf894ff50ca
mariru27 <marina.rusu.99@gmail.com>
2020-12-24 09:42:55 +0200

    Add-Migration OneToMany and update database

---
+++ Trustme/Migrations/20201224074206_oneToManyRelationship.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Trustme.Migrations
+{
+    public partial class oneToManyRelationship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Key",
+                table: "Key");
+
+            migrationBuilder.AddColumn<int>(
+                name: "KeyId",
+                table: "Key",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Key",
+                table: "Key",
+                column: "KeyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Key_UserId",
+                table: "Key",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Key",
+                table: "Key");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Key_UserId",
+                table: "Key");
+
+            migrationBuilder.DropColumn(
+                name: "KeyId",
+                table: "Key");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Key",
+                table: "Key",
+                column: "UserId");
+        }
+    }
+}
