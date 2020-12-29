7110ebc7bfb4c28273092bb954885e839f16b7b0
mariru27 <marina.rusu.99@gmail.com>
2020-12-29 11:45:16 +0200

    added migration keySize

---
+++ Trustme/Migrations/20201229094246_Added_keySize.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Trustme.Migrations
+{
+    public partial class Added_keySize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "certificateName",
+                table: "Key",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PublicKey",
+                table: "Key",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "keySize",
+                table: "Key",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "keySize",
+                table: "Key");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "certificateName",
+                table: "Key",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PublicKey",
+                table: "Key",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
