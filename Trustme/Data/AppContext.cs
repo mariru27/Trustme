695de9c9ecfb5f2967835f4664caa181fd7d7050
mariru27 <marina.rusu.99@gmail.com>
2021-01-08 14:31:47 +0200

    added in role in context

--- Trustme/Data/AppContext.cs
+++ Trustme/Data/AppContext.cs
@@ -17,11 +17,14 @@ namespace Trustme.Data
 
         public DbSet<User> User {set; get;}
         public DbSet<Key> Key { set; get; }
+
+        public DbSet<Role> Role { set; get; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>().ToTable("User");
 
             modelBuilder.Entity<Key>().ToTable("Key");
+            modelBuilder.Entity<Role>().ToTable("Role");
         }
 
     }
