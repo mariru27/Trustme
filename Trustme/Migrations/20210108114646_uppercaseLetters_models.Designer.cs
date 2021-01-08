8cd137eeca330c0b1cb774c3100ef6c71bc9f1b2
mariru27 <marina.rusu.99@gmail.com>
2021-01-08 13:48:35 +0200

    added migration Uppercase letters in models

--- Trustme/Migrations/20210108114646_uppercaseLetters_models.Designer.cs
+++ Trustme/Migrations/20210108114646_uppercaseLetters_models.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Trustme.Data;
 
 namespace Trustme.Migrations
 {
     [DbContext(typeof(AppContext))]
-    partial class AppContextModelSnapshot : ModelSnapshot
+    [Migration("20210108114646_uppercaseLetters_models")]
+    partial class uppercaseLetters_models
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
