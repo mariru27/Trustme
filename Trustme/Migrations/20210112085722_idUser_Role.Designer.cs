f4f35918354609b32f59c7a41e536fe9df646ff2
mariru27 <marina.rusu.99@gmail.com>
2021-01-12 11:01:43 +0200

    added users list in role class

--- Trustme/Migrations/20210112085722_idUser_Role.Designer.cs
+++ Trustme/Migrations/20210112085722_idUser_Role.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Trustme.Data;
 
 namespace Trustme.Migrations
 {
     [DbContext(typeof(AppContext))]
-    partial class AppContextModelSnapshot : ModelSnapshot
+    [Migration("20210112085722_idUser_Role")]
+    partial class idUser_Role
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -118,7 +120,7 @@ namespace Trustme.Migrations
             modelBuilder.Entity("Trustme.Models.User", b =>
                 {
                     b.HasOne("Trustme.Models.Role", "Role")
-                        .WithMany("Users")
+                        .WithMany()
                         .HasForeignKey("RoleName");
                 });
 #pragma warning restore 612, 618
---
+++ Trustme/Migrations/20210112085722_idUser_Role.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Trustme.Migrations
+{
+    public partial class idUser_Role : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IdUser",
+                table: "Role",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdUser",
+                table: "Role");
+        }
+    }
+}
