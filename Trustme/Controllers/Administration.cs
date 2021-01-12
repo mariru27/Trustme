fe8587e295a1257ea7703518f8adfd7141139639
mariru27 <marina.rusu.99@gmail.com>
2021-01-12 12:19:57 +0200

    add role(obj, make relationship between user and role) in user table

--- Trustme/Controllers/Administration.cs
+++ Trustme/Controllers/Administration.cs
@@ -164,7 +164,10 @@ namespace Trustme.Controllers
             }
             if (ModelState.IsValid && user.Password == user.ConfirmPassword)
             {
-
+                Role role = _context.Role.Where(a => a.RoleName == user.UserRole).FirstOrDefault();
+                //role.IdUser = user.UserId;
+                role.Users.Add(user);
+                user.Role = role;
                 _context.Add(user);
                 await _context.SaveChangesAsync();
                 return  await LogIn(user.Username, user.Password);
