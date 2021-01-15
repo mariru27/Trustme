65d5a09099101849a7b2cb66485ca0c4e4ea8993
mariru27 <marina.rusu.99@gmail.com>
2021-01-15 10:56:43 +0200

    AdminContext

---
+++ Trustme/Data/AdminContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Trustme.Models;
+
+
+namespace Trustme.Data
+{
+    public class AdminContext : DbContext
+    {
+        public AdminContext(DbContextOptions<AdminContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> User { set; get; }
+        public DbSet<Role> Role { set; get; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("User");
+
+            modelBuilder.Entity<Role>().ToTable("Role");
+        }
+    }
+}
