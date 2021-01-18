a7b6efc1d5071c33adac9930712aa0fc6a2dab8c
mariru27 <marina.rusu.99@gmail.com>
2021-01-18 13:49:33 +0200

    after adding UserRole(Role object), in migration is generated UserRoleIdRole FK(database updated)

--- Trustme/Migrations/20210118114243_UserRole.Designer.cs
+++ Trustme/Migrations/20210118114243_UserRole.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Trustme.Data;
 
 namespace Trustme.Migrations
 {
     [DbContext(typeof(AppContext))]
-    partial class AppContextModelSnapshot : ModelSnapshot
+    [Migration("20210118114243_UserRole")]
+    partial class UserRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
