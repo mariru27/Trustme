a47700cb7bd9cc71ebfe73def14c204d3954293b
mariru27 <marina.rusu.99@gmail.com>
2021-01-18 15:20:58 +0200

    added User and Role in RUViewModel file

--- Trustme/ViewModels/RolesUserViewModel.cs
+++ Trustme/ViewModels/RolesUserViewModel.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Trustme.Models;
 
 namespace Trustme.ViewModels
 {
     public class RolesUserViewModel
     {
-
+        public User User { get; set; }
+        public Role Role { get; set; }
     }
 }
