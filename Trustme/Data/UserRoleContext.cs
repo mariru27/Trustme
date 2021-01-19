6beb384e1ce09bc9d8cca4378167248a7d156d7a
mariru27 <marina.rusu.99@gmail.com>
2021-01-19 16:57:51 +0200

    Add UserRoleContext in data

---
+++ Trustme/Data/UserRoleContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Trustme.Models;
+
+
+namespace Trustme.Data
+{
+    public class UserRoleContext : DbContext
+    {
+        public UserRoleContext(DbContextOptions<UserRoleContext> options) : base(options) {  }
+
+        public DbSet<User> User { set; get; }
+        public DbSet<Role> Role { set; get; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("User");
+
+            modelBuilder.Entity<Role>().ToTable("Role");
+        }
+    }
+}
