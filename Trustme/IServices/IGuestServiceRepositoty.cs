039bf69275ec80950ff49122091e9ed36eef33e9
mariru27 <marina.rusu.99@gmail.com>
2021-01-19 16:51:35 +0200

    add register and login(in interface and class that imp that)

--- Trustme/IServices/IGuestServiceRepositoty.cs
+++ Trustme/IServices/IGuestServiceRepositoty.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Trustme.Models;
 
 namespace Trustme.IServices
 {
     interface IGuestServiceRepositoty
     {
         //register and login
+        User LogIn(User user);
+        User Register(User user);
     }
 }
--- Trustme/Service/GuestServiceRepository.cs
+++ Trustme/Service/GuestServiceRepository.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Trustme.IServices;
+using Trustme.Models;
 
 namespace Trustme.Service
 {
-    public class GuestServiceRepository
+    public class GuestServiceRepository : IGuestServiceRepositoty
     {
+        public User LogIn(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Register(User user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
