9f9a177261a572db6cced95c13f2aed275bf057e
mariru27 <marina.rusu.99@gmail.com>
2021-01-28 12:31:42 +0200

    Added RoleContext

---
+++ Trustme/Data/RoleContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Trustme.Models;
+
+namespace Trustme.Data
+{
+    public class RoleContext : DbContext
+    {
+        public RoleContext(DbContextOptions<RoleContext> options) : base(options) { }
+        public DbSet<Role> Role { set; get; }
+        public DbSet<User> User { set; get; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("User");
+            modelBuilder.Entity<User>().ToTable("Role");
+
+        }
+
+    }
+}
