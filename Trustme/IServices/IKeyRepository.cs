aef808889d442373e630c2b9262e96c415e664be
mariru27 <marina.rusu.99@gmail.com>
2021-01-28 13:14:31 +0200

    added IKeyRepository and functions

---
+++ Trustme/IServices/IKeyRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Trustme.Models;
+
+namespace Trustme.IServices
+{
+    interface IKeyRepository
+    {
+        public void add();
+        public void delete();
+        public void update();
+        public Enumerable listAllKeys(User user);
+
+    }
+}
