75078c8bc2c1353efe089bb316f52c9ad779fe16
mariru27 <marina.rusu.99@gmail.com>
2021-01-28 13:16:32 +0200

    added IUserRepository and functions

---
+++ Trustme/IServices/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Trustme.Models;
+
+namespace Trustme.IServices
+{
+    interface IUserRepository
+    {
+        public void add();
+        public void delete();
+        public void update();
+        public Enumerable listAllUsers();
+    }
+}
