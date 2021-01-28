e5b62d7095cb625364605d1d84221d1690e1f367
mariru27 <marina.rusu.99@gmail.com>
2021-01-28 13:21:09 +0200

    implemented interface IRoleRepository in RoleRepository

--- Trustme/IServices/IRoleRepository.cs
+++ Trustme/IServices/IRoleRepository.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Trustme.Models;
 
 namespace Trustme.IServices
 {
     interface IRoleRepository
     {
-
+        public void add();
+        public void delete();
+        public void update();
+        public IEnumerable<User> listAllUsers(Role role);
     }
 }
--- Trustme/Service/RoleRepository.cs
+++ Trustme/Service/RoleRepository.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Trustme.IServices;
+using Trustme.Models;
 
 namespace Trustme.Service
 {
     public class RoleRepository : IRoleRepository
     {
+        public void add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> listAllUsers(Role role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void update()
+        {
+            throw new NotImplementedException();
+        }
+
+
     }
 }
