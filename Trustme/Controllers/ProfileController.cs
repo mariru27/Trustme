6fa8b42cfe7f37f7dbd46baa69b8aaee5c93629e
mariru27 <marina.rusu.99@gmail.com>
2021-02-02 16:09:03 +0200

    added controllers

---
+++ Trustme/Controllers/ProfileController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Trustme.Controllers
+{
+    //change phpto, edit all values 
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
