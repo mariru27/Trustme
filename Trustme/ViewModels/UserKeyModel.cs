78cd636358ad80996e02e09c54b25403ee424bb6
mariru27 <marina.rusu.99@gmail.com>
2021-02-02 14:01:47 +0200

    added UserKeyModel

---
+++ Trustme/ViewModels/UserKeyModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Trustme.Models;
+
+namespace Trustme.ViewModels
+{
+    public class UserKeyModel
+    {
+        public Key key;
+        public User user;
+    }
+}
