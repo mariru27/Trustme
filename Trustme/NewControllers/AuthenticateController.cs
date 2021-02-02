de4d300dcf41ecc035e1f4359fdde4685b1cccbb
mariru27 <marina.rusu.99@gmail.com>
2021-02-02 16:29:22 +0200

    changed new Controllers location

---
+++ Trustme/NewControllers/AuthenticateController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Trustme.Controllers
+{   
+    //register, login
+    public class AuthenticateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
