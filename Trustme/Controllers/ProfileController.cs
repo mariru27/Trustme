6693bd03197c03306815b86f00f23d7ac17c6219
mariru27 <marina.rusu.99@gmail.com>
2021-02-08 12:25:03 +0200

    added HttpRequestFunctions and all methods that take httpContext parameter

---
+++ Trustme/Controllers/ProfileController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Trustme.NewControllers
+{
+    //change phpto, edit all values, display
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [Authorize]
+        public async Task<IActionResult> Profile()
+        {
+            string username = this.getUsername(HttpContext);
+            ViewData["user"] = _context.User.Where(a => a.Username == username).SingleOrDefault();
+            ViewData["keys"] = await _context.Key.Where(a => a.UserKeyId == this.getUserId(HttpContext)).ToListAsync();
+            return View();
+        }
+    }
+}
---
+++ Trustme/Service/HttpRequestFunctions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trustme.Service
+{
+    public class HttpRequestFunctions
+    {
+        public IEnumerable<Key> getAllKeys(HttpContext httpContext)
+        {
+            var appContext = _context.Key.Where(k => k.UserKeyId == this.getUserId(httpContext)).AsEnumerable();
+            return appContext;
+        }
+
+        public string getPublicKey(HttpContext httpcontext)
+        {
+            var username = this.getUsername(httpcontext);
+            User user = _context.User.Where(a => a.Username == username)?.SingleOrDefault();
+            Key key = _context.Key.Where(a => a.UserKeyId == user.UserId)?.SingleOrDefault();
+            return key.PublicKey;
+
+        }
+
+        public string getPublicKey(HttpContext httpcontext, int certificateId)
+        {
+            var username = this.getUsername(httpcontext);
+            Key key = _context.Key.Where(a => a.UserKeyId == this.getUserId(httpcontext) && a.KeyId == certificateId).SingleOrDefault();
+            return key.PublicKey;
+
+        }
+
+        public bool isloggedIn(HttpContext httpcontext)
+        {
+            var username = httpcontext.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+            if (username != null)
+                return true;
+            return false;
+        }
+
+        public string getUsername(HttpContext httpcontext)
+        {
+            return httpcontext.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+        }
+
+        public int getUserId(HttpContext httpcontext)
+        {
+            string username = this.getUsername(httpcontext);
+            User user = _context.User.Where(a => a.Username == username)?.FirstOrDefault();
+            return user.UserId;
+
+        }
+    }
+}
