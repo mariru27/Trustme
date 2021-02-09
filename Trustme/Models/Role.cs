138dd2295a220466177edd8496909b1a43feab48
mariru27 <marina.rusu.99@gmail.com>
2021-02-09 14:21:26 +0200

    copy constructor in role model class

--- Trustme/Models/Role.cs
+++ Trustme/Models/Role.cs
@@ -8,6 +8,12 @@ namespace Trustme.Models
 {
     public class Role
     {
+        public Role() { }
+        public Role(Role role) 
+        {
+            this.IdRole = role.IdRole;
+            this.RoleName = role.RoleName;
+        }
         [Key]
         public int IdRole { get; set; }
         public string RoleName { get; set; }
