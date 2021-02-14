962ab15cdd278dc982450c82de5175575aeafaec
mariru27 <marina.rusu.99@gmail.com>
2021-02-14 13:00:31 +0200

    add GenerateCertificateController

--- Trustme/Controllers/GenerateCertificateController.cs
+++ Trustme/Controllers/GenerateCertificateController.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
-namespace Trustme.NewControllers
+namespace Trustme.Controllers
 {
     public class GenerateCertificateController : Controller
     {
