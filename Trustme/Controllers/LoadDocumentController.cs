a0902710724260e5195a238aee7040383d48589a
mariru27 <marina.rusu.99@gmail.com>
2021-02-18 14:32:49 +0200

    Added LoadDocumentController

---
+++ Trustme/Controllers/LoadDocumentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trustme.Controllers
+{
+    public class LoadDocumentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+   
+    }
+}
