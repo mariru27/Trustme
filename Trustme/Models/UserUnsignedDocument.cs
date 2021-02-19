a068dc7b114b7de7916558b9430597e601e0037b
mariru27 <marina.rusu.99@gmail.com>
2021-02-19 19:14:59 +0200

    Added UnsignedDocument and UserUnsignedDocument

---
+++ Trustme/Models/UserUnsignedDocument.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trustme.Models
+{
+    public class UserUnsignedDocument
+    {
+        [Key]
+        public int IdUserUnsignedDocument { get; set; }
+        public int UserId { get; set; }
+        public int UnsignedDocumentId { get; set; }
+    }
+}
