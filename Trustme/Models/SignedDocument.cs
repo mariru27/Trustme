394b1cbbbbc4734e22894169ba0755a4aac62890
mariru27 <marina.rusu.99@gmail.com>
2021-02-19 19:20:11 +0200

    Added SignedDocument

---
+++ Trustme/Models/SignedDocument.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trustme.Models
+{
+    public class SignedDocument
+    {
+        public int IdSignedDocument { get; set; }
+        public int KeyId { get; set; }
+        public IFormFile Document { get; set; }
+        public string Signature { get; set; }
+        public string Comment { get; set; }
+    }
+}
