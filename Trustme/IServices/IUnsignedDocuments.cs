3a455ea81311ec27fb45d41ab32dba9d1a808247
mariru27 <marina.rusu.99@gmail.com>
2021-02-20 14:38:43 +0200

    Added IUnsignedDocumentsRepository and UnsignedDocumentRepository, interface registered

---
+++ Trustme/IServices/IUnsignedDocuments.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Trustme.Models;
+
+namespace Trustme.IServices
+{
+    interface IUnsignedDocuments
+    {
+        public UnsignedDocument AddUnsignedDocument(UnsignedDocument unsignedDocument);  
+    }
+}
