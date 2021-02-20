d0ed67c2cdb697d12055fdfdd6a8b3d47769ff14
mariru27 <marina.rusu.99@gmail.com>
2021-02-20 15:24:31 +0200

    Implemented LoadDocumentToSign post method

--- Trustme/Controllers/LoadDocumentController.cs
+++ Trustme/Controllers/LoadDocumentController.cs
@@ -3,11 +3,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Trustme.Models;
+using Trustme.ViewModels;
+using Trustme.Service;
+using Trustme.IServices;
 
 namespace Trustme.Controllers
 {
     public class LoadDocumentController : Controller
     {
+        private IUserRepository _UserRepository;
+        private IUnsignedDocumentRepository _UnsignedDocumentRepository;
+
+        public LoadDocumentController(IUserRepository userRepository, IUnsignedDocumentRepository unsignedDocumentRepository)
+        {
+            _UserRepository = userRepository;
+            _UnsignedDocumentRepository = unsignedDocumentRepository;
+                
+        }
         public IActionResult Index()
         {
             return View();
@@ -15,6 +28,19 @@ namespace Trustme.Controllers
 
         public IActionResult LoadDocumentToSign()
         {
+            return View();
+        }
+
+        public IActionResult LoadDocumentToSign(DocumentModel documentModel)
+        {
+            UserUnsignedDocument userUnsignedDocument = new UserUnsignedDocument();
+            //unsignedDocument.Document = documentModel.Document;
+            userUnsignedDocument.UnsignedDocument.KeyPreference = documentModel.CertificateName;
+            if(_UserRepository.GetUserbyUsername(documentModel.Username) != null)
+            {
+                userUnsignedDocument.User = _UserRepository.GetUserbyUsername(documentModel.Username);
+
+            }
             return View();
         }
     }
