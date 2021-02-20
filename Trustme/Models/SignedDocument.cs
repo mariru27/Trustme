b49f9793854b081bc91ac84d06a5bff2cfb1150e
mariru27 <marina.rusu.99@gmail.com>
2021-02-20 14:13:20 +0200

    Last changes to models

--- Trustme/Models/SignedDocument.cs
+++ Trustme/Models/SignedDocument.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,12 @@ namespace Trustme.Models
 {
     public class SignedDocument
     {
+        [Key]
         public int IdSignedDocument { get; set; }
-        public int KeyId { get; set; }
-        public Key Key { get; set; }
-        public IFormFile Document { get; set; }
+
+        public byte[] Document { get; set; }
         public string Signature { get; set; }
         public string Comment { get; set; }
+
     }
 }
--- Trustme/Models/UserUnsignedDocument.cs
+++ Trustme/Models/UserUnsignedDocument.cs
@@ -11,6 +11,7 @@ namespace Trustme.Models
         [Key]
         public int IdUserUnsignedDocument { get; set; }
         public int UserId { get; set; }
+        public User User { get; set; }
         public int UnsignedDocumentId { get; set; }
         public UnsignedDocument UnsignedDocument { get; set; }
     }
