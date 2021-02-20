db999b40ccca03a764bc77a3d540dd3eff331f0a
mariru27 <marina.rusu.99@gmail.com>
2021-02-20 14:16:54 +0200

    added migrations

--- Trustme/Migrations/20210220121608_DocumentsTables.Designer.cs
+++ Trustme/Migrations/20210220121608_DocumentsTables.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Trustme.Data;
 
 namespace Trustme.Migrations
 {
     [DbContext(typeof(AppContext))]
-    partial class AppContextModelSnapshot : ModelSnapshot
+    [Migration("20210220121608_DocumentsTables")]
+    partial class DocumentsTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
