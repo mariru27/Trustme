07966a735f68dba6cac6fea2e14977847a4012d4
mariru27 <marina.rusu.99@gmail.com>
2021-02-21 13:55:46 +0200

    Modifications in UnsignedDocument Model, added name property

--- Trustme/Migrations/20210221115353_name_unsigneddocument.Designer.cs
+++ Trustme/Migrations/20210221115353_name_unsigneddocument.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Trustme.Data;
 
 namespace Trustme.Migrations
 {
     [DbContext(typeof(AppContext))]
-    partial class AppContextModelSnapshot : ModelSnapshot
+    [Migration("20210221115353_name_unsigneddocument")]
+    partial class name_unsigneddocument
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Trustme/Migrations/20210221115353_name_unsigneddocument.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Trustme.Migrations
+{
+    public partial class name_unsigneddocument : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "UnsignedDocuments",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "UnsignedDocuments");
+        }
+    }
+}
