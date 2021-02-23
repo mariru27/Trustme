3b8380d39dbdaf2e39f22f1cf4df7dd3175b0e13
mariru27 <marina.rusu.99@gmail.com>
2021-02-23 12:51:14 +0200

    Delete FK SignDoument from Key Table

--- Trustme/Migrations/20210223104921_deleteFk_SignedDocument_fomKeyTable.Designer.cs
+++ Trustme/Migrations/20210223104921_deleteFk_SignedDocument_fomKeyTable.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Trustme.Data;
 
 namespace Trustme.Migrations
 {
     [DbContext(typeof(AppContext))]
-    partial class AppContextModelSnapshot : ModelSnapshot
+    [Migration("20210223104921_deleteFk_SignedDocument_fomKeyTable")]
+    partial class deleteFk_SignedDocument_fomKeyTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
