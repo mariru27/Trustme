726b336d838bf799a7242158c8dc6a7823120c20
mariru27 <marina.rusu.99@gmail.com>
2021-02-23 13:07:34 +0200

    Add Name property in SignedTable

---
+++ Trustme/Migrations/20210223110515_name_SignedDocumentTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Trustme.Migrations
+{
+    public partial class name_SignedDocumentTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "SignedDocuments",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "SignedDocuments");
+        }
+    }
+}
