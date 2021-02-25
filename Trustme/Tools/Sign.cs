d96baf9b25c40c2f5917a794713544905c5fd4bb
mariru27 <marina.rusu.99@gmail.com>
2021-02-25 09:50:48 +0200

    Add Tools and ITools(folder), implement classes for refactoring

---
+++ Trustme/Tools/Sign.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Trustme.ITools;
+
+namespace Trustme.Tools
+{
+    public class Sign : ISign
+    {
+        
+    }
+}
