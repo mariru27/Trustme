e528009de17fa9ecb2fac78e98457088d7fe76aa
mariru27 <marina.rusu.99@gmail.com>
2021-02-25 11:52:37 +0200

    Add ICertificate and Certificate

---
+++ Trustme/Tools/Certificate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Trustme.ITools;
+
+namespace Trustme.Tools
+{
+    public class Certificate : ICertificate
+    {
+    }
+}
