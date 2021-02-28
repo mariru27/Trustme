0ed2c1fddad7a4c1b4ed43b1d6d7267c2d95c4e3
mariru27 <marina.rusu.99@gmail.com>
2021-02-28 11:01:09 +0200

    Added SignModel

---
+++ Trustme/Tools/ToolsModels/SignModel.cs
@@ -0,0 +1,17 @@
+﻿using Org.BouncyCastle.Crypto;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trustme.Tools.ToolsModels
+{
+    public class SignModel
+    {
+        AsymmetricKeyParameter privatekeyy;
+        byte[] fileBytesdoc;
+        StreamReader reader;
+        string keypath;
+    }
+}
