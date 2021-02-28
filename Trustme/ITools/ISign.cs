a796bd68334c382a3b24476a21bc8a3838a133a6
mariru27 <marina.rusu.99@gmail.com>
2021-02-28 12:13:05 +0200

    Used VerifySignature() in VerifySignatureDocument

--- Trustme/ITools/ISign.cs
+++ Trustme/ITools/ISign.cs
@@ -12,7 +12,7 @@ namespace Trustme.ITools
     {
         public SignModel SignDoc(IFormFile pkfile, IFormFile docfile, int certificates, HttpContext httpContext);
         public string SignDocument(SignModel signModel);
-        public ISigner VerifySignature();
+        public ISigner VerifySignature(VerifySignatureModel verifySignatureModel);
 
     }
 }
