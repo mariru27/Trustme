2c4c17614542edf1f01d0fc1ef420091305295cb
mariru27 <marina.rusu.99@gmail.com>
2021-03-03 13:32:39 +0200

    Added migrations

---
+++ Trustme/Migrations/20210303112957_usernamesent_signed.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Trustme.Migrations
+{
+    public partial class usernamesent_signed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SentFromUsername",
+                table: "UnsignedDocuments",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SignedByUsername",
+                table: "SignedDocuments",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SentFromUsername",
+                table: "UnsignedDocuments");
+
+            migrationBuilder.DropColumn(
+                name: "SignedByUsername",
+                table: "SignedDocuments");
+        }
+    }
+}
