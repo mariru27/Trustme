7c5cf947ba048110328484c9e8bb061a6841eb42
mariru27 <marina.rusu.99@gmail.com>
2021-03-03 12:10:50 +0200

    Injected AppContext in SignedDocumentRepository, added AddSignedDocument()

--- Trustme/Service/SignedDocumentRepository.cs
+++ Trustme/Service/SignedDocumentRepository.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Trustme.IServices;
+using Trustme.Models;
+using Trustme.Data;
 
 namespace Trustme.Service
 {
     public class SignedDocumentRepository : ISignedDocumentRepository
     {
+        private AppContext _context;
+        public SignedDocumentRepository(AppContext appContext)
+        {
+            _context = appContext;
+        }
+        public bool AddSignedDocument(SignedDocument signedDocument)
+        {
+            
+            return true;
+        }
     }
 }
