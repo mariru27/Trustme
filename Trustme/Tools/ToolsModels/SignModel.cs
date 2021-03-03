f268b6459a4ccfd497560c1d00f3d8ddcfea0d45
mariru27 <marina.rusu.99@gmail.com>
2021-03-03 11:19:22 +0200

    Added PKNull error

--- Trustme/Tools/ToolsModels/SignModel.cs
+++ Trustme/Tools/ToolsModels/SignModel.cs
@@ -15,5 +15,6 @@ namespace Trustme.Tools.ToolsModels
         public string keypath { set; get; }
         public bool verifytest { set; get; }
         public bool validKey { set; get; }
+       
     }
 }
