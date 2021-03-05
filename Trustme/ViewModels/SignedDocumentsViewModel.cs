eb639e56c540db0431ed22162bdbf9607f53f7b9
mariru27 <marina.rusu.99@gmail.com>
2021-03-05 12:00:55 +0200

    Added SignedDocumentsViewModel to pass SignedDocuments data to view (without Document value)

---
+++ Trustme/ViewModels/SignedDocumentsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Trustme.Models;
+
+namespace Trustme.ViewModels
+{
+    public class SignedDocumentsViewModel
+    {
+
+        public int IdSignedDocument { get; set; }
+        public string Name { get; set; }
+        public string SignedByUsername { get; set; }
+        public string Signature { get; set; }
+        public int KeyId { get; set; }
+        public Key Key { get; set; }
+    }
+}
