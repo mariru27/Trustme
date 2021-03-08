4a779e3d8df45b2cacc148aa205cdeabc51f6d0b
mariru27 <marina.rusu.99@gmail.com>
2021-03-08 12:19:59 +0200

    Change password length in database

--- Trustme/Models/User.cs
+++ Trustme/Models/User.cs
@@ -38,7 +38,6 @@ namespace Trustme.Models
         public string Username { set; get; }
 
         [Required]
-        [StringLength(50, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 6)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
