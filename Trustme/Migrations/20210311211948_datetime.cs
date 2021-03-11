ef7d66b773502d5a9796edab28c41f15bb514995
mariru27 <marina.rusu.99@gmail.com>
2021-03-11 23:23:14 +0200

    add-migration

---
+++ Trustme/Migrations/20210311211948_datetime.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Trustme.Migrations
+{
+    public partial class datetime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SentTime",
+                table: "UnsignedDocuments",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SentTime",
+                table: "SignedDocuments",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SignedTime",
+                table: "SignedDocuments",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SentTime",
+                table: "UnsignedDocuments");
+
+            migrationBuilder.DropColumn(
+                name: "SentTime",
+                table: "SignedDocuments");
+
+            migrationBuilder.DropColumn(
+                name: "SignedTime",
+                table: "SignedDocuments");
+        }
+    }
+}
