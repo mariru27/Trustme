331273fe8bfd856cff0de674b75a9986ee575f53
mariru27 <marina.rusu.99@gmail.com>
2021-03-17 12:32:05 +0200

    Delete authorize admin test

--- Trustme/Controllers/SignDocumentsController.cs
+++ Trustme/Controllers/SignDocumentsController.cs
@@ -35,7 +35,6 @@ namespace Trustme.Controllers
             _SignedDocumentRepository = signedDocumentRepository;
             _UserRepository = userRepository;
         }
-        [Authorize(Roles = "Admin")]
         public IActionResult UnsignedDocuments()
         {
             User user = new User();
