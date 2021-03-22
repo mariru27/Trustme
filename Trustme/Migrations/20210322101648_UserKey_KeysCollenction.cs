d53a2a58ecb77e9547385200377d1c5fa089b2cb
mariru27 <marina.rusu.99@gmail.com>
2021-03-22 12:24:07 +0200

    Added migration, got error "To change the IDENTITY property of a column, the column needs to be dropped and recreated"

---
+++ Trustme/Migrations/20210322101648_UserKey_KeysCollenction.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Trustme.Migrations
+{
+    public partial class UserKey_KeysCollenction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserKey_Key_IdUserKey",
+                table: "UserKey");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "IdUserKey",
+                table: "UserKey",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserKeyIdUserKey",
+                table: "Key",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Key_UserKeyIdUserKey",
+                table: "Key",
+                column: "UserKeyIdUserKey");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Key_UserKey_UserKeyIdUserKey",
+                table: "Key",
+                column: "UserKeyIdUserKey",
+                principalTable: "UserKey",
+                principalColumn: "IdUserKey",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Key_UserKey_UserKeyIdUserKey",
+                table: "Key");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Key_UserKeyIdUserKey",
+                table: "Key");
+
+            migrationBuilder.DropColumn(
+                name: "UserKeyIdUserKey",
+                table: "Key");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "IdUserKey",
+                table: "UserKey",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int))
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserKey_Key_IdUserKey",
+                table: "UserKey",
+                column: "IdUserKey",
+                principalTable: "Key",
+                principalColumn: "KeyId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
