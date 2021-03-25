dc962761b4ab2be124c801a00c3529a11c1fb001
mariru27 <marina.rusu.99@gmail.com>
2021-03-25 12:58:18 +0200

    remove authorize from verify page

--- Trustme/Controllers/VerifySignatureController.cs
+++ Trustme/Controllers/VerifySignatureController.cs
@@ -11,7 +11,6 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace Trustme.Controllers
 {
-    [Authorize(Roles = "Pro, User, Free")]
     public class VerifySignatureController : Controller
     {
         private IHostingEnvironment Environment;
