ac560dcfc200d17e21e03c11635eaa0096572f08
mariru27 <marina.rusu.99@gmail.com>
2021-03-25 12:19:19 +0200

    Delete some unused code

--- Trustme/Data/InitDB.cs
+++ Trustme/Data/InitDB.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using System.Linq;
+﻿using System.Linq;
 using Trustme.Controllers;
 using Trustme.IServices;
 using Trustme.Models;
@@ -9,19 +8,10 @@ namespace Trustme.Data
 
     public class InitDB
     {
-        private readonly IHttpRequestFunctions _HttpRequestFunctions;
-        private AuthenticateController _AuthenticateController;
-        public InitDB(IHttpRequestFunctions httpRequestFunctions, AuthenticateController authenticateController)
-        {
-            _HttpRequestFunctions = httpRequestFunctions;
-            _AuthenticateController = authenticateController;
-        }
-
-
         public static void InitDb(AppContext context)
         {
 
-            
+
             context.Database.EnsureCreated();
 
 
@@ -46,35 +36,6 @@ namespace Trustme.Data
                 context.SaveChanges();
             }
 
-            //var users = new User[]
-            //{
-            //    new User{UserKeyId = 1, FirstName = "Mihai", SecondName="Popescu", Mail="mihaipopescu@gmail.com", Username="mihai12345", Password = "password", ConfirmPassword = "password", UserRole = roles[0] },
-            //    new User{UserKeyId = 2, FirstName = "Mihai", SecondName="Popescu", Mail="mihaipopescu@gmail.com", Username="mariru", Password = "marina", ConfirmPassword = "passwor", UserRole = roles[0]  }
-            //};
-
-            //users[0].UserRole = roles[0];
-            //users[1].UserRole = roles[0];
-            //roles[0].Users.Add(users[0]);
-            //roles[0].Users.Add(users[1]);
-
-            //var keys = new Key[] 
-            //{
-            //     new Key{ KeyId = 1, UserKeyId = 1, PublicKey = " ", CertificateName = "testeCertificate1", Description = "test description", KeySize = 1024, User = users[0]},     
-            //     new Key{ KeyId = 2, UserKeyId = 2,PublicKey = " ", CertificateName = "testeCertificate2", Description = "test description3", KeySize = 1024, User = users[1]},     
-            //};
-
-            //foreach (User u in users)
-            //{
-            //    context.User.Add(u);
-            //}
-            //context.SaveChanges();
-
-            //foreach (Key k in keys)
-            //{
-            //    context.Key.Add(k);
-            //}
-
-            // context.SaveChanges();
         }
     }
 }
