147f64ce3c695398d32918e7dd3ae2ad626de5f6
mariru27 <marina.rusu.99@gmail.com>
2021-03-31 17:03:53 +0300

    Add some properties to UploadDocumentModel

--- Trustme/ViewModels/UploadDocumentModel.cs
+++ Trustme/ViewModels/UploadDocumentModel.cs
@@ -1,11 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Trustme.Models;
 
 namespace Trustme.ViewModels
 {
     public class UploadDocumentModel
     {
-        public IEnumerable<Key> Keys;
-        public string Username;
+        public IEnumerable<Key> Keys { get; set; }
+        public string Username { get; set; }
+        [Required]
+        public string CertificateName { get; set; }
+        [Required]
+        public IFormFile Document { get; set; }
     }
 }
