c2ad8f5b4433aedd7ac405bc7f68b43293ddbe9f
mariru27 <marina.rusu.99@gmail.com>
2021-04-02 10:00:35 +0300

    Add editUserModel

---
+++ Trustme/ViewModels/EditUserModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Trustme.Models;
+
+namespace Trustme.ViewModels
+{
+    public class EditModel
+    {
+        public int UserId { set; get; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Name must contain just letters")]
+        [Display(Name = "First Name")]
+
+        public string FirstName { set; get; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Name must contain just letters")]
+        [Display(Name = "Second Name")]
+        public string SecondName { set; get; }
+
+        [Required]
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "This is not a valid mail")]
+        [Display(Name = "Mail")]
+        public string Mail { set; get; }
+
+        [Required]
+        [RegularExpression("^(?!.{26})[a-zA-Z0-9]+?$", ErrorMessage = "In username field is allowing just letters or numbers")]
+        [Display(Name = "Username")]
+        public string Username { set; get; }
+        public int RoleId { get; set; }
+
+        public Role Role { get; set; }
+
+
+    }
+}
