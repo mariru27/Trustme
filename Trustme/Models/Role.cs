edc669fec67b01839527ec08c386608a8616ea9a
mariru27 <marina.rusu.99@gmail.com>
2021-04-13 12:00:25 +0300

    Display role(not roleid) , verify sign - secundary card

--- Trustme/Models/Role.cs
+++ Trustme/Models/Role.cs
@@ -5,12 +5,13 @@ namespace Trustme.Models
     public class Role
     {
         public Role() { }
-        public Role(Role role) 
+        public Role(Role role)
         {
             this.IdRole = role.IdRole;
             this.RoleName = role.RoleName;
         }
         [Key]
+        [Display(Name = "Role")]
         public int IdRole { get; set; }
         public string RoleName { get; set; }
     }
