ee7efbfca4c635e51754cde111141556db8f00c7
mariru27 <marina.rusu.99@gmail.com>
2021-04-15 17:14:40 +0300

    Give a name to result file!!!

--- Trustme/Controllers/SignDocumentsController.cs
+++ Trustme/Controllers/SignDocumentsController.cs
@@ -43,10 +43,7 @@ namespace Trustme.Controllers
         public IActionResult Download(int id)
         {
             var document = _UnsignedDocumentRepository.GetUnsignedDocumentById(id);
-
-            var result = new FileContentResult(document.Document, "text/plain");
-
-            return result;
+            return File(document.Document, document.ContentType, document.Name);
         }
 
         [HttpGet]
