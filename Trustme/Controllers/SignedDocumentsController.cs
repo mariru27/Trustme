664c8b98c51b0a8df68ff323ba49808a7237a506
mariru27 <marina.rusu.99@gmail.com>
2021-04-19 14:58:23 +0300

    SignedDocumentDetails action

--- Trustme/Controllers/SignedDocumentsController.cs
+++ Trustme/Controllers/SignedDocumentsController.cs
@@ -29,8 +29,11 @@ namespace Trustme.Controllers
             return RedirectToAction("SignedDocumentsFromUsers");
         }
 
-        public IActionResult SignedDocumentDetails(SignedDocumentsViewModel signedDocumentsViewModel)
+        public IActionResult SignedDocumentDetails(int id)
         {
+            SignedDocument signedDocument = _SignedDocumentRepository.GetSignedDocumentById(id);
+            SignedDocumentsViewModel signedDocumentsViewModel = new SignedDocumentsViewModel(signedDocument);
+            signedDocumentsViewModel.KeyName = _KeyRepository.GetKeyById(signedDocument.KeyId).CertificateName;
             return View(signedDocumentsViewModel);
         }
         public IActionResult Download(int id)
