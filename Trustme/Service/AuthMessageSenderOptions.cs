22b20b621a2f06e3327b81f7736376d0f11165e6
mariru27 <marina.rusu.99@gmail.com>
2021-04-21 15:50:47 +0300

    create AuthMessageSenderOptions

---
+++ Trustme/Service/AuthMessageSenderOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Trustme.Service
+{
+    public class AuthMessageSenderOptions
+    {
+        public string SendGridUser { get; set; }
+        public string SendGridKey { get; set; }
+    }
+}
