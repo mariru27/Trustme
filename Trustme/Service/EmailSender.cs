62a11be95f6e8e35af89a667201bf5141094facf
mariru27 <marina.rusu.99@gmail.com>
2021-04-22 15:34:05 +0300

    connect to SMTP server

--- Trustme/Service/EmailSender.cs
+++ Trustme/Service/EmailSender.cs
@@ -1,10 +1,17 @@
-﻿using MimeKit;
+﻿using MailKit.Net.Smtp;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
 using Trustme.IServices;
 
 namespace Trustme.Service
 {
     public class EmailSender : IEmailSender
     {
+        private readonly IConfiguration _Configuration;
+        public EmailSender(IConfiguration iConfig)
+        {
+            _Configuration = iConfig;
+        }
         public void SendMail(string messageContent, string toUserNameMail)
         {
             MimeMessage message = new MimeMessage();
@@ -19,6 +26,16 @@ namespace Trustme.Service
 
             message.Subject = "This is email test subject";
 
+            BodyBuilder bodyBuilder = new BodyBuilder();
+            bodyBuilder.HtmlBody = "<h1>Hello World!</h1>";
+            bodyBuilder.TextBody = "Hello World!";
+
+            //Connect and authenticate with the SMTP server
+            SmtpClient client = new SmtpClient();
+            client.Connect("timetrustme99@gmail.com", 587, true);
+
+            //email password is stored in my local.json file
+            client.Authenticate("timetrustme99@gmail.com", _Configuration.GetSection("SMTPpassword").Value);
         }
     }
 }
