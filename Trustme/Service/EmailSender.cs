cc84acbd0a6ce0d79224dcc1ff4ee3c7e43929e4
mariru27 <marina.rusu.99@gmail.com>
2021-04-22 11:23:24 +0300

    Install mail kit

--- Trustme/Service/EmailSender.cs
+++ Trustme/Service/EmailSender.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System.Threading.Tasks;
 using Trustme.IServices;
 
@@ -7,12 +9,12 @@ namespace Trustme.Service
 {
     public class EmailSender : IEmailSender
     {
+        public AuthMessageSenderOptions Options { get; } //set only via Secret Manager
         public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor)
         {
             Options = optionsAccessor.Value;
         }
 
-        public AuthMessageSenderOptions Options { get; } //set only via Secret Manager
 
         public Task SendEmailAsync(string email, string subject, string message)
         {
