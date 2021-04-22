57cc9adbeb1764b141548293a367fb9f7b9be797
mariru27 <marina.rusu.99@gmail.com>
2021-04-22 11:37:57 +0300

    send to

--- Trustme/Service/EmailSender.cs
+++ Trustme/Service/EmailSender.cs
@@ -5,12 +5,20 @@ namespace Trustme.Service
 {
     public class EmailSender : IEmailSender
     {
-        public void SendMail(string messageContent, string toUserName)
+        public void SendMail(string messageContent, string toUserNameMail)
         {
             MimeMessage message = new MimeMessage();
+
             MailboxAddress from = new MailboxAddress("Admin",
                                     "timetrustme99@gmail.com");
+            message.From.Add(from);
 
-        };
+            MailboxAddress to = new MailboxAddress("user",
+                                    "marina.rusu.99@gmail.com");
+            message.To.Add(to);
+
+            message.Subject = "This is email test subject";
+
+        }
     }
 }
