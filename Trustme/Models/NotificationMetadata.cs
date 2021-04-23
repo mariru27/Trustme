2d7152eb08c9a4c4d0601523e0497fafe40a71f4
mariru27 <marina.rusu.99@gmail.com>
2021-04-23 11:04:05 +0300

    Add NotificationMetadata class

---
+++ Trustme/Models/NotificationMetadata.cs
@@ -0,0 +1,12 @@
+﻿namespace Trustme.Models
+{
+    public class NotificationMetadata
+    {
+        public string Sender { get; set; }
+        public string SmtpServer { get; set; }
+        public int Port { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
