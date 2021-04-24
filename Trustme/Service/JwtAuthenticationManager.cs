970d8f0f86c16c915c2ba29ed99f075283c54b4c
mariru27 <marina.rusu.99@gmail.com>
2021-04-24 10:08:06 +0300

    get privateKey in bytes

--- Trustme/Service/JwtAuthenticationManager.cs
+++ Trustme/Service/JwtAuthenticationManager.cs
@@ -1,18 +1,28 @@
 ﻿
 using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using Trustme.Models;
 
 namespace Trustme.Service
 {
     public class JwtAuthenticationManager
     {
         private readonly IConfiguration _Configuration;
-        private readonly string privateKey;
         public JwtAuthenticationManager(IConfiguration configuration)
         {
             _Configuration = configuration;
-            privateKey = _Configuration.GetValue<string>("PrivateKey");
         }
 
+
+        public string Authenticate(User user)
+        {
+
+            var tockenHandler = new JwtSecurityTokenHandler();
+
+            var privateKey = _Configuration.GetValue<string>("PrivateKey");
+            var
+
+        }
 
     }
 }
