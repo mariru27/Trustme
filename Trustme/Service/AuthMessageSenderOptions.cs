46dc52a446ff0744708ced0f0d857824b2359572
mariru27 <marina.rusu.99@gmail.com>
2021-04-24 09:44:00 +0300

    delete unused class AuthMess..

--- Trustme/Service/AuthMessageSenderOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Trustme.Service
-{
-    public class AuthMessageSenderOptions
-    {
-        public string SendGridUser { get; set; }
-        public string SendGridKey { get; set; }
-    }
-}
