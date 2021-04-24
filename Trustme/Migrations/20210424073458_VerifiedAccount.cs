86984a790917678df8577cdfcc3be3c478177dd6
mariru27 <marina.rusu.99@gmail.com>
2021-04-24 10:36:00 +0300

    add migration VerifiedAccount

---
+++ Trustme/Migrations/20210424073458_VerifiedAccount.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Trustme.Migrations
+{
+    public partial class VerifiedAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "VerifiedAccount",
+                table: "User",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VerifiedAccount",
+                table: "User");
+        }
+    }
+}
