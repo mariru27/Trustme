979eafe8aa8400f3e2f14ab2b4cb9d242dc03462
mariru27 <marina.rusu.99@gmail.com>
2021-05-08 12:00:47 +0300

    make seen, interface

--- Trustme/IServices/ISignedDocumentRepository.cs
+++ Trustme/IServices/ISignedDocumentRepository.cs
@@ -12,5 +12,7 @@ namespace Trustme.IServices
         public IEnumerable<SignedDocument> Search_ListAllSignedDocumentsSentFromUsername(User user, string Username);
         public IEnumerable<SignedDocument> Search_ListAllSignedDocumentsSignedByUsername(User user, string Username);
         public IEnumerable<SignedDocument> Search_ListAllSignedDocumentsSentFromUsername_SignedByUsername(User user, string SentFromUsername, string SignedByUsername);
+        public void MakeSeen(User user);
+
     }
 }
