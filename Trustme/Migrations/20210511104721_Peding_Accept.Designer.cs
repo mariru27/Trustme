e05651546eb954ab70d7b356ce1efe12636355c1
mariru27 <marina.rusu.99@gmail.com>
2021-05-11 13:48:28 +0300

    add-migration

--- Trustme/Migrations/20210511104721_Peding_Accept.Designer.cs
+++ Trustme/Migrations/20210511104721_Peding_Accept.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Trustme.Data;
 
 namespace Trustme.Migrations
 {
     [DbContext(typeof(Data.AppContext))]
-    partial class AppContextModelSnapshot : ModelSnapshot
+    [Migration("20210511104721_Peding_Accept")]
+    partial class Peding_Accept
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
