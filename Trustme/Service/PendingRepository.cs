7789dda7a960cd699aa8e5d513f88079303e8c3a
mariru27 <marina.rusu.99@gmail.com>
2021-05-12 12:53:15 +0300

    Set TimeAcceptedPendingRequest

--- Trustme/Service/PendingRepository.cs
+++ Trustme/Service/PendingRepository.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using Trustme.Data;
 using Trustme.IServices;
 using Trustme.Models;
 
@@ -9,8 +9,8 @@ namespace Trustme.Service
 {
     public class PendingRepository : IPendingRepository
     {
-        private AppContext _context;
-        public PendingRepository(AppContext appContext)
+        private Data.AppContext _context;
+        public PendingRepository(Data.AppContext appContext)
         {
             _context = appContext;
         }
@@ -20,7 +20,7 @@ namespace Trustme.Service
             return _context.User.Where(a => a.UserId == user.UserId).Join(_context.Pendings,
                 u => u.UserId,
                 p => p.User.UserId,
-                (u, p) => new Pending { TimePendingRequest = p.TimePendingRequest, User = p.User, UsernameWhoSentPending = p.UsernameWhoSentPending })
+                (u, p) => new Pending { TimeSentPendingRequest = p.TimeSentPendingRequest, User = p.User, UsernameWhoSentPending = p.UsernameWhoSentPending })
                 .ToList();
         }
 
@@ -29,13 +29,14 @@ namespace Trustme.Service
             var pending = _context.User.Where(a => a.UserId == user.UserId).Join(_context.Pendings,
             u => u.UserId,
             p => p.User.UserId,
-            (u, p) => new Pending { TimePendingRequest = p.TimePendingRequest, User = p.User, UsernameWhoSentPending = p.UsernameWhoSentPending })
+            (u, p) => new Pending { TimeSentPendingRequest = p.TimeSentPendingRequest, User = p.User, UsernameWhoSentPending = p.UsernameWhoSentPending })
             .ToList().Where(u => u.UsernameWhoSentPending == username).SingleOrDefault();
 
             if (pending != null)
             {
                 //User accept pending from username
                 pending.Accepted = true;
+                pending.TimeAcceptedPendingRequest = DateTime.Now;
 
                 //update and save changes
                 //_context.Update(pending);
