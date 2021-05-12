74f50a2740115978fc601daf39f0836d29ab6682
mariru27 <marina.rusu.99@gmail.com>
2021-05-12 11:56:20 +0300

    implemented ListAllPedingRequests

--- Trustme/Service/PendingRepository.cs
+++ Trustme/Service/PendingRepository.cs
@@ -1,4 +1,8 @@
-﻿using Trustme.Data;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Trustme.Data;
+using Trustme.Models
+
 
 
 namespace Trustme.Service
@@ -9,6 +13,15 @@ namespace Trustme.Service
         public PendingRepository(AppContext appContext)
         {
             _context = appContext;
+        }
+
+        public IEnumerable<PendingRequest> ListAllPedingRequests(User user)
+        {
+            _context.User.Where(a => a.UserId == user.UserId).Join(_context.PendingRequest,
+                u => u.UserId,
+                p => p.User.UserId,
+                (u, p) => new PendingRequest { TimePendingRequest = p.TimePendingRequest, User = p.User, Username = p.Username })
+                .ToList();
         }
     }
 }
