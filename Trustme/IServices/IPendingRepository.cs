189e7d081730e9a8527300c913cc330b69b08ff9
mariru27 <marina.rusu.99@gmail.com>
2021-05-13 13:44:03 +0300

    interface

--- Trustme/IServices/IPendingRepository.cs
+++ Trustme/IServices/IPendingRepository.cs
@@ -8,5 +8,7 @@ namespace Trustme.IServices
         public IEnumerable<Pending> ListAllPendingRequests(User user);
         public void MarkUserAcceptPendingFromUsername(User user, string username);
         public bool CheckAcceptedPendingFromUsername(User user, string username);
+        public void AddPendingRequest(User user, string UsernameWhoSentPending);
+
     }
 }
