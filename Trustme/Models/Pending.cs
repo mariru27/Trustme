666a778e5d83a24661ba5b4e5b483fcaca212d30
mariru27 <marina.rusu.99@gmail.com>
2021-05-16 17:06:56 +0300

    add Blocked property in pending model

--- Trustme/Models/Pending.cs
+++ Trustme/Models/Pending.cs
@@ -15,6 +15,7 @@ namespace Trustme.Models
         public int IdPedingUsers { get; set; }
         public string UsernameWhoSentPending { get; set; }
         public bool Accepted { get; set; }
+        public bool Blocked { get; set; }
         public DateTime TimeSentPendingRequest { get; set; }
         public DateTime TimeAcceptedPendingRequest { get; set; }
         public User User { get; set; }
